b03e30761b53b5bf789454cf4f07b82a52884a4d
zhangxu <1850037517@163.com>
2017-04-01 15:35:03 +0800

    变更脚本名字

--- userID/snapshotWork.py
+++ userID/snapshotWork.py
@@ -3,13 +3,22 @@ import os
 import shutil
 
 print("ConnectPhone")
-device=MonkeyRunner.waitForConnection()
+device = MonkeyRunner.waitForConnection()
 
 Num = 0
-userfilepath = str('D:\userID\userID')
-bookfilepath = str('D:\userID\\bookID')
-#print(userfilepath)
-#print(bookfilepath)
+rootPath = 'F:\Work\workScript\userID'
+userfilepath = str(rootPath + '\\userID')
+bookfilepath = str(rootPath + '\\bookID')
+
+if os.path.exists(str(rootPath) + '\\Log'):
+	print(str(rootPath) + '\\Log' + "file is exist")
+else:
+	os.mkdir(str(rootPath) + '\\Log')
+
+if os.path.exists(str(rootPath) + '\\Success'):
+	print(str(rootPath) + '\\Success' + "file is exist")
+else:
+	os.mkdir(str(rootPath) + '\\Success')
 
 def start():
 	device.shell('am force-stop com.itangyuan')
@@ -31,6 +40,22 @@ def start():
 	MonkeyRunner.sleep(3)
 	device.touch(633, 1265, 'DOWN_AND_UP')
 	MonkeyRunner.sleep(1)
+	
+	result = device.takeSnapshot()
+	pic = result.getSubImage((0, 50, 720, 1230))
+	MonkeyRunner.sleep(2)	
+
+	resultTruePath = str(rootPath) + '\example\update.png'
+	resultTrue=MonkeyRunner.loadImageFromFile(resultTruePath)
+
+	if(pic.sameAs(resultTrue,0.9)):
+		print("")
+		device.touch(198, 1060, 'DOWN_AND_UP')
+	else:
+		print("1No")
+		MonkeyRunner.sleep(10)
+		device.touch(198, 1060, 'DOWN_AND_UP')
+		
 	print("start_My")
 	device.drag((360,300),(360,1000),0.5,3)
 	MonkeyRunner.sleep(1)
@@ -53,7 +78,7 @@ def exitUser():
 	MonkeyRunner.sleep(3)
 	device.touch(300, 1200, 'DOWN_AND_UP')
 	print("=========================reStart=========================")
-	MonkeyRunner.sleep(2)
+	MonkeyRunner.sleep(4)
 	device.press('KEYCODE_BACK ', 'DOWN_AND_UP')	
 	MonkeyRunner.sleep(2)
 	device.drag((360,300),(360,1000),0.5,3)
@@ -66,13 +91,13 @@ while True:
 		Num = 0
 		userIDA = userID.split('_')[1]
 		userIDA = str(userIDA)
-		print("userID==========",userID)
+		print("userID==========", userID)
 		#print("userIDA==========",userIDA)
-		userIDPath = userfilepath + "/" + userID
+		userIDPath = userfilepath + "\\" + userID
 		fulluserIDpath = str(userIDPath)
 
 		#================= 1 ==================================
-		device.touch(323, 335, 'DOWN_AND_UP')
+		device.touch(323, 298, 'DOWN_AND_UP')
 		MonkeyRunner.sleep(2)
 		try:
 			device.type(userIDA)
@@ -90,36 +115,40 @@ while True:
 		print("snapshotOne")
 		result = device.takeSnapshot()
 		pic = result.getSubImage((0, 50, 720, 1230))
-		MonkeyRunner.sleep(2)	
+		MonkeyRunner.sleep(2)
 		
-		userFile = 'D:\\userID\\snapshotUser\\' + userID
-		userFile = str(userFile)
-		print("userFile===",userFile)
-		if os.path.exists(userFile):
+		saveSnapshotPath = rootPath + '\\snapshotUser\\' + userID
+		saveSnapshotPath = str(saveSnapshotPath)
+
+		print("saveSnapshotPath===", saveSnapshotPath)
+		if os.path.exists(saveSnapshotPath):
 			print("file is exist")
 		else:
-			os.mkdir(userFile)
-		#save onePic	
-		resultTrue=MonkeyRunner.loadImageFromFile(r"D:\userID\example\1.png")
+			os.mkdir(saveSnapshotPath)
+		#save onePic
+		resultTruePath = str(rootPath) + "\\example\\1.png"
+		print('resultTruePath===', resultTruePath)
+		resultTrue = MonkeyRunner.loadImageFromFile(resultTruePath)
 		
 		if(pic.sameAs(resultTrue,0.8)):
 			print("1YES")
-			WritePath = userFile + '\\' + Numa+ '_' + userIDA + '.png'
+			logPath = str(rootPath) + '\\Log\\log.txt'
+			log = open(logPath, "a")
+			log.write("1YES"+"\n")
+			log.close()
+			WritePath = saveSnapshotPath + '\\' + Numa + '_' + userIDA + '.png'
 			pic.writeToFile(WritePath, "png")
 		else:
 			print("1No")
-			start()
-			# MonkeyRunner.sleep(10)
-			# device.press('KEYCODE_BACK', MonkeyDevice.DOWN_AND_UP)
-			# MonkeyRunner.sleep(3)
-			# device.press('KEYCODE_BACK', MonkeyDevice.DOWN_AND_UP)
-			# MonkeyRunner.sleep(3)
-			# device.press('KEYCODE_BACK', MonkeyDevice.DOWN_AND_UP)
-			# MonkeyRunner.sleep(3)
+			logPath = str(rootPath) + '\\Log\\log.txt'
+			log = open(logPath, "a")
+			log.write("1No"+"\n")
+			log.close()
+			start()	
 			Num = 0
 			exitUser()
-			break		
-			
+			break
+
 		#================= 2 ==================================
 		device.touch(350, 350, 'DOWN_AND_UP')
 		MonkeyRunner.sleep(10)
@@ -131,22 +160,30 @@ while True:
 		pic = result.getSubImage((0, 50, 720, 1230))
 		MonkeyRunner.sleep(2)
 		#save twoPic
-		resultTrue=MonkeyRunner.loadImageFromFile(r"D:\userID\example\2.png")
+		resultTruePath = str(rootPath) + "\\example\\2.png"
+		resultTrue=MonkeyRunner.loadImageFromFile(resultTruePath)
 		
 		if(pic.sameAs(resultTrue,0.1)):
 			print("2YES")
-			WritePath = userFile + '\\' + Numa+ '_' + userIDA + '.png'
+			logPath = str(rootPath) + '\\Log\\log.txt'
+			log = open(logPath, "a")
+			log.write("2YES"+"\n")
+			log.close()
+			WritePath = saveSnapshotPath + '\\' + Numa + '_' + userIDA + '.png'
 			pic.writeToFile(WritePath, "png")
 			MonkeyRunner.sleep(1)
 			device.press('KEYCODE_BACK', MonkeyDevice.DOWN_AND_UP)
 			MonkeyRunner.sleep(2)
 		else:
 			print("2No")
+			logPath = str(rootPath) + '\\Log\\log.txt'
+			log = open(logPath, "a")
+			log.write("2No"+"\n")
+			log.close()
 			start()
 			Num = 0
 			exitUser()
 			break
-
 		
 		#================= 3 ==================================
 		device.touch(620, 500, 'DOWN_AND_UP')
@@ -159,11 +196,16 @@ while True:
 		pic = result.getSubImage((0, 50, 720, 1230))
 		MonkeyRunner.sleep(2)
 		#save threePic
-		resultTrue=MonkeyRunner.loadImageFromFile(r"D:\userID\example\3.png")
+		resultTruePath = str(rootPath) + "\\example\\3.png"
+		resultTrue=MonkeyRunner.loadImageFromFile(resultTruePath)
 			
 		if(pic.sameAs(resultTrue,0.4)):
 			print("3YES")
-			WritePath = userFile + '\\' + Numa+ '_' + userIDA + '.png'
+			logPath = str(rootPath) + '\\Log\\log.txt'
+			log = open(logPath, "a")
+			log.write("3YES"+"\n")
+			log.close()
+			WritePath = saveSnapshotPath + '\\' + Numa + '_' + userIDA + '.png'
 			pic.writeToFile(WritePath, "png")
 			MonkeyRunner.sleep(1)
 			device.press('KEYCODE_BACK', MonkeyDevice.DOWN_AND_UP)
@@ -172,13 +214,16 @@ while True:
 			MonkeyRunner.sleep(1)
 		else:
 			print("3No")
+			logPath = str(rootPath) + '\\Log\\log.txt'
+			log = open(logPath, "a")
+			log.write("3No"+"\n")
+			log.close()
 			start()
 			Num = 0
 			exitUser()
 			break
 		
 		#================= 4 ==================================
-		
 		for bookID in os.listdir(bookfilepath):
 			bookIDA = bookID.split('_')[1]
 			bookIDA = str(bookIDA)
@@ -200,7 +245,7 @@ while True:
 			device.touch(220, 780, 'DOWN_AND_UP')
 			MonkeyRunner.sleep(1)	
 			#shutil.move(fullbookIDpath, r'C:\Users\Work\Desktop\Success')
-			break		
+			break
 		Num = Num + 1
 		print("Num==", Num)
 		Numa = str(Num)
@@ -209,15 +254,24 @@ while True:
 		pic = result.getSubImage((0, 50, 720, 1230))
 		MonkeyRunner.sleep(2)
 		#save FourPic
-		resultTrue=MonkeyRunner.loadImageFromFile(r"D:\userID\example\4.png")
+		resultTruePath = str(rootPath) + "\\example\\4.png"
+		resultTrue=MonkeyRunner.loadImageFromFile(resultTruePath)
 		
 		if(pic.sameAs(resultTrue,0.1)):
 			print("4YES")
-			WritePath = userFile + '\\' + Numa+ '_' + userIDA + '.png'
+			logPath = str(rootPath) + '\\Log\\log.txt'
+			log = open(logPath, "a")
+			log.write("4YES"+"\n")
+			log.close()
+			WritePath = saveSnapshotPath + '\\' + Numa + '_' + userIDA + '.png'
 			pic.writeToFile(WritePath, "png")
 			MonkeyRunner.sleep(1)
 		else:
 			print("4No")
+			logPath = str(rootPath) + '\\Log\\log.txt'
+			log = open(logPath, "a")
+			log.write("4No"+"\n")
+			log.close()
 			start()
 			Num = 0
 			exitUser()
@@ -225,7 +279,7 @@ while True:
 		
 		#================= 5 ==================================
 		device.touch(500, 800, 'DOWN_AND_UP')
-		MonkeyRunner.sleep(20)	
+		MonkeyRunner.sleep(20)
 		Num = Num + 1
 		print("Num==", Num)
 		Numa = str(Num)
@@ -234,22 +288,30 @@ while True:
 		pic = result.getSubImage((0, 50, 720, 1230))
 		MonkeyRunner.sleep(2)
 		#save threePic
-		resultTrue=MonkeyRunner.loadImageFromFile(r"D:\userID\example\5.png")
+		resultTruePath = str(rootPath) + "\\example\\5.png"
+		resultTrue=MonkeyRunner.loadImageFromFile(resultTruePath)
 		
 		if(pic.sameAs(resultTrue,0.1)):
 			print("5YES")
-			WritePath = userFile + '\\' + Numa+ '_' + userIDA + '.png'
+			logPath = str(rootPath) + '\\Log\\log.txt'
+			log = open(logPath, "a")
+			log.write("5YES"+"\n")
+			log.close()
+			WritePath = saveSnapshotPath + '\\' + Numa + '_' + userIDA + '.png'
 			pic.writeToFile(WritePath, "png")
 			MonkeyRunner.sleep(1)	
 			device.press('KEYCODE_BACK', MonkeyDevice.DOWN_AND_UP)
 			MonkeyRunner.sleep(2)
 		else:
 			print("5No")
+			logPath = str(rootPath) + '\\Log\\log.txt'
+			log = open(logPath, "a")
+			log.write("5No"+"\n")
+			log.close()
 			MonkeyRunner.sleep(150)
-			
 			if(pic.sameAs(resultTrue,0.1)):
 				print("RE5YES")
-				WritePath = userFile + '\\' + Numa+ '_' + userIDA + '.png'
+				WritePath = saveSnapshotPath + '\\' + Numa + '_' + userIDA + '.png'
 				pic.writeToFile(WritePath, "png")
 				MonkeyRunner.sleep(1)
 				device.press('KEYCODE_BACK', MonkeyDevice.DOWN_AND_UP)
@@ -275,14 +337,23 @@ while True:
 		pic = result.getSubImage((0, 50, 720, 1230))
 		MonkeyRunner.sleep(2)
 		#save SixePic
-		resultTrue=MonkeyRunner.loadImageFromFile(r"D:\userID\example\\6.png")
+		resultTruePath = str(rootPath) + "\\example\\6.png"
+		resultTrue=MonkeyRunner.loadImageFromFile(resultTruePath)
 		
 		if(pic.sameAs(resultTrue,0.1)):
 			print("6YES")
-			WritePath = userFile + '\\' + Numa+ '_' + userIDA + '.png'
+			logPath = str(rootPath) + '\\Log\\log.txt'
+			log = open(logPath, "a")
+			log.write("6YES"+"\n")
+			log.close()
+			WritePath = saveSnapshotPath + '\\' + Numa+ '_' + userIDA + '.png'
 			pic.writeToFile(WritePath, "png")
 		else:
 			print("6No")
+			logPath = str(rootPath) + '\\Log\\log.txt'
+			log = open(logPath, "a")
+			log.write("6No"+"\n")
+			log.close()
 			MonkeyRunner.sleep(100)
 			device.press('KEYCODE_BACK', MonkeyDevice.DOWN_AND_UP)
 			MonkeyRunner.sleep(3)
@@ -293,7 +364,8 @@ while True:
 			Num = 0
 			exitUser()
 			break
-		MonkeyRunner.sleep(2)	
+
+		MonkeyRunner.sleep(2)
 		device.press('KEYCODE_BACK', MonkeyDevice.DOWN_AND_UP)
 		MonkeyRunner.sleep(2)
 		device.press('KEYCODE_BACK', MonkeyDevice.DOWN_AND_UP)
@@ -301,11 +373,9 @@ while True:
 		device.press('KEYCODE_BACK', MonkeyDevice.DOWN_AND_UP)
 		MonkeyRunner.sleep(2)
 		
-		shutil.move(fullbookIDpath, r'C:\Users\Work\Desktop\Success')
-		shutil.move(fulluserIDpath, r'C:\Users\Work\Desktop\Success')
-		print("MovefullbookIDpath===",fullbookIDpath)
-		print("MovefullbookIDpath",fulluserIDpath)
-		
+		shutil.move(fullbookIDpath, str(rootPath + '\\Success'))
+		shutil.move(fulluserIDpath, str(rootPath + '\\Success'))
+		print("MovefulluserIDpath===", str(rootPath + '\\Success'))
+		print("fulluserIDpath", fulluserIDpath)
 		MonkeyRunner.sleep(2)
-		
 		exitUser()
